204bc217d68fc99364822f54aa112ec77c98f48c
Romario Gustave <romagustave10@gmail.com>
2021-01-25 07:17:40 +0000

    Automatically backed up by Learn

--- app/serializers/sighting_serializer.rb
+++ app/serializers/sighting_serializer.rb
@@ -1,4 +1,9 @@
 class SightingSerializer
   include FastJsonapi::ObjectSerializer
   attributes 
+  ApplicationController
+  def show
+    sighting = Sighting.find(params[:id])
+    render json: SightingSerializer.new(sighting)
+  end
 end
